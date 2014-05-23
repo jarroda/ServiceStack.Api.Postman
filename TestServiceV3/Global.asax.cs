002ab99e98e5f3b29cbd4b7a707fda8ca8bc27a7
Jarrod Alexander <jarrod.alexander@gmail.com>
2014-05-22 22:10:51 -0500

    Added a test project to use for development

---
+++ TestServiceV3/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using ServiceStack.WebHost.Endpoints;
+using ServiceStack.OrmLite;
+using System.IO;
+using ServiceStack.Api.Postman;
+using ServiceStack.ServiceInterface.Cors;
+
+namespace FluentMigrator.ServiceStack.TestV3
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public class AppHost : AppHostBase
+        {
+            public AppHost() : base("Test Web Services", typeof(AppHost).Assembly) { }
+
+            public override void Configure(Funq.Container container)
+            {
+                Plugins.Add(new CorsFeature());
+                Plugins.Add(new PostmanFeature());
+            }
+        }
+
+        protected void Application_Start()
+        {
+            new AppHost().Init();
+        }
+    }
+}
---
+++ TestServiceV3/TestService.cs
@@ -0,0 +1,43 @@
+﻿using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FluentMigrator.ServiceStack.TestV3
+{
+    public class TestService : Service
+    {
+        public object Get(Contact contact)
+        {
+            return null;
+        }
+
+        public object Post(Contact contact)
+        {
+            return null;
+        }
+
+        public object Delete(Contact contact)
+        {
+            return null;
+        }
+
+        public object Put(Contact contact)
+        {
+            return null;
+        }
+    }
+
+    [Route("/contacts", "GET,PUT")]
+    [Route("/contacts/{Id}", "GET,POST,DELETE")]
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
